983e2f3314eed48e362923c47673434c74e83915
André <up201905770@edu.fc.up.pt>
2022-07-23 16:20:50 +0100

    Clean

--- analise/teste1_diff_work/create.py
+++
@@ -1,8 +0,0 @@
-import os
-
-t_size = [1, 10, 100, 1000, 10000, 100000]
-work_size = [7, 8, 9, 10, 11]
-
-for t in t_size:
-    for w in work_size:
-        os.system("touch f" + str(t) + "_" + str(w))
